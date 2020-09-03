3f4d3a901ad4dc6ec5ec055fff09662851a90c2e
Lucas Martins <luf.martins@globo.com>
2020-09-03 20:43:00 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import { db } from './models/index.js';
+import { gradeRouter } from './routes/gradeRouter.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+(async () => {
+  try {
+    await db.mongoose.connect(db.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('conectado ao mongodb');
+  } catch (error) {
+    console.log(process.env)
+    console.log('erro ao conectar ao mongodb');
+    process.exit();
+  }
+})();
+
+const app = express();
+
+//define o dominio de origem para consumo do servico
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: 'http://localhost:8080',
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send('API em execucao');
+});
+
+app.use(gradeRouter);
+
+app.listen( process.env.PORT, () => {
+  console.log('api em execução')
+});
